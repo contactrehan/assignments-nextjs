c930482d9224ac4e6387b8e88a395ee4323da525
contactrehan <contactrehan2209@gmail.com>
2023-11-18 23:43:19 +0500

    clone piaic

---
+++ first-assignment/app/tags.tsx
@@ -0,0 +1,67 @@
+
+// <div>
+//         {/* All Html Tags */}
+//         {/* Headings */}
+//         <h1>Heading 1</h1>
+//         <h2>Heading 2</h2>
+//         <h3>Heading 3</h3>
+//         <h4>Heading 4</h4>
+//         <h5>Heading 5</h5>
+//         <h6>Heading 6</h6>
+//       </div>
+//       {/* Paragraph tag */}
+//       <p>
+//         This paragraph contains a lot of lines in the source code, but the
+//         browser ignores it.
+//       </p>
+//       {/* Bold*/}
+//       <b>This text is bold</b>
+//       {/* Strong */}
+//       <strong>This text is important!</strong>
+//       {/* italic */}
+//       <i>This text is italic</i>
+//       {/* Emphasize */}
+//       <em>This text is emphasized</em>
+//       {/* Smaller text */}
+//       <small>This is some smaller text.</small>
+//       {/* Mark */}
+//       <p>Do not forget to buy <mark>milk</mark> today.</p>
+//     {/* del */}
+//     <p>My favorite color is <del>blue</del> red.</p>
+//     {/* ins */}
+//     <p>My favorite color is <del>blue</del> <ins>red</ins>.</p>
+//     {/* sub */}
+//     <p>This is <sub>subscripted</sub> text.</p>
+//     {/*sup  */}
+//     <p>This is <sup>superscripted</sup> text.</p>
+//     <div>
+      
+//       {/* Quotation */}
+//       <p>Here is a quote from WWF's website:</p>
+// <blockquote cite="http://www.worldwildlife.org/who/index.html">
+// For 60 years, WWF has worked to help people and nature thrive. As the world's leading conservation organization, WWF works in nearly 100 countries. At every level, we collaborate with people around the world to develop and deliver innovative solutions that protect communities, wildlife, and the places in which they live.
+// </blockquote>
+
+//         <div>
+//     {/* <!-- Write your comments here --> */}
+
+//     {/* a tag */}
+//     <a href="https://www.w3schools.com/" target="_blank">Visit W3Schools.com!</a>
+    
+//         </div>
+
+//         <video src="assets/video/y2mate.com - Which laptop or PC is recommended for React Native development_1080p.mp4"></video>
+//       </div>
+//       <div>
+//         <a href="https://www.youtube.com/shorts/krGewbkk3G4" target="/new">
+//           youtube
+//         </a>
+//         <iframe
+//           width="320"
+//           height="560"
+//           src="https://www.youtube.com/embed/krGewbkk3G4"
+//           title="📱 Mobile App Development: 🚀 Turn Your Ideas into Reality 🎉"
+//           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+//         ></iframe>
+//       </div>
+// )}
