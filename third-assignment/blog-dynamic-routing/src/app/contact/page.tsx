3a90ff8b601b3b5da44ae133bc001f51a1bc4e0e
contactrehan <140440920+contactrehan@users.noreply.github.com>
2023-12-02 23:19:20 +0500

    contact

--- third-assignment/blog-dynamic-routing/src/app/contact/page.tsx
+++ third-assignment/blog-dynamic-routing/src/app/contact/page.tsx
@@ -5,8 +5,8 @@ export default function Contact()
             <h2>Contact Me</h2>
             <p>Feel free to reach out to me through the following channels:</p>
             <ul>
-                <li>Email: example@example.com</li>
-                <li>Twitter: @example_twitter</li>
+                <li>Email: contactrehan2209@gmail.com</li>
+                <li>Twitter: contactrehan</li>
             </ul>
         </section>
     )
