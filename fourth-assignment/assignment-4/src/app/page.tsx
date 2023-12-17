a844c9206ed91d3c41c0e4b98a7facf132ce3acc
contactrehan <contactrehan2209@gmail.com>
2023-12-17 09:55:32 +0500

    body tag duplication removed from page.tsx

--- fourth-assignment/assignment-4/src/app/page.tsx
+++ fourth-assignment/assignment-4/src/app/page.tsx
@@ -4,7 +4,7 @@ import "./components/next_button"
 import Next_button from './components/next_button';
 let Home = ()=>{
   return (
-   <body>
+   <>
     <div 
      className="text-center mt-10  bg-image" 
      >
@@ -12,7 +12,7 @@ let Home = ()=>{
 <h1 className="text-4xl font-bold mb-6 text-black">On Click Button for Next and Previous Name</h1>
 <Next_button/>
     </div>
-   </body>)
+   </>)
 }
 export default Home;
 
